412a8f25db780ba88f9a665694aaaf5d47c646fe
Manu Awasthi <manu.awasthi1508@gmail.com>
2025-12-18 18:35:21 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,37 +1,11 @@
 import express from 'express'
-import dotenv from 'dotenv'
-import connectDB from './database/db.js'; 
+import dotenv from 'dotenv' 
+import connectDB from './database/db.js';
 import userRoutes from './Routes/userRoutes.js'
-
 
 const app = express();
 app.use(express.json())
 dotenv.config();
-import express from "express";
-import dotenv from "dotenv";
-import connectDB from "./database/db.js";
-import userRoutes from "./routes/userRoutes.js"; 
-
-dotenv.config(); 
-
-const app = express();
-app.use(express.json());
-
-connectDB();
-
-
-app.get("/", (req, res) => {
-  res.send("User CRUD API is running 🚀");
-});
-
-app.use("/api/users", userRoutes);
-
-const PORT = process.env.PORT || 5000;
-
-
-app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Server running on port ${PORT}`);
-});
 
 connectDB();
 
@@ -39,4 +13,4 @@ app.get("/" , (req , res)=>{`server runing at port ${process.env.PORT}`})
 app.use("api/users" , userRoutes);
 const port = process.env.PORT || 5000
 
-app.listen(port , ()=>{console.log("port is listening");})
+app.listen(port , ()=>{console.log("port is listening");}) i have this in server.js still not working why?
