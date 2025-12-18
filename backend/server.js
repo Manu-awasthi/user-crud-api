41d11fffb9ac1598a78691d855a8e96334b4503f
Manu Awasthi <manu.awasthi1508@gmail.com>
2025-12-18 18:39:36 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -13,4 +13,4 @@ app.get("/" , (req , res)=>{`server runing at port ${process.env.PORT}`})
 app.use("api/users" , userRoutes);
 const port = process.env.PORT || 5000
 
-app.listen(port , ()=>{console.log("port is listening");}) i have this in server.js still not working why?
+app.listen(port , ()=>{console.log("port is listening");})
