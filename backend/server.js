527d306c97404c8100e039267dcfb143efb409f6
Manu Awasthi <manu.awasthi1508@gmail.com>
2025-12-18 18:31:42 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,37 @@
 import express from 'express'
-import dotenv from 'dotenv' 
-import connectDB from './database/db.js';
+import dotenv from 'dotenv'
+import connectDB from './database/db.js'; 
 import userRoutes from './Routes/userRoutes.js'
+
 
 const app = express();
 app.use(express.json())
 dotenv.config();
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./database/db.js";
+import userRoutes from "./routes/userRoutes.js"; 
+
+dotenv.config(); 
+
+const app = express();
+app.use(express.json());
+
+connectDB();
+
+
+app.get("/", (req, res) => {
+  res.send("User CRUD API is running 🚀");
+});
+
+app.use("/api/users", userRoutes);
+
+const PORT = process.env.PORT || 5000;
+
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
 connectDB();
 
